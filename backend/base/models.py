d750c5bc091a690988850c0ef2545d6178f374e6
Jas802 <jas.spencer414@gmail.com>
2021-05-12 10:47:03 -0400

    finished building models

--- backend/base/models.py
+++ backend/base/models.py
@@ -67,3 +67,19 @@ class OrderItem(models.Model):
 
     def __str__(self):
         return str(self.name)
+
+
+class ShippingAddress(models.Model):
+    order = models.OneToOneField(
+        Order, on_delete=models.CASCADE, null=True, blank=True)
+    address = models.CharField(max_length=200, null=True, blank=True)
+    city = models.CharField(max_length=200, null=True, blank=True)
+    postalCode = models.CharField(max_length=200, null=True, blank=True)
+    country = models.CharField(max_length=200, null=True, blank=True)
+    shippingPrice = models.IntegerField(null=True, blank=True, default=0)
+    price = models.DecimalField(
+        max_digits=7, decimal_places=2, null=True, blank=True)
+    _id = _id = models.AutoField(primary_key=True, editable=False)
+
+    def __srt__(self):
+        return str(self.address)
