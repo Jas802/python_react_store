4ef1e75c8d5e279cb2498ff94c17d857082d9626
Jas802 <jas.spencer414@gmail.com>
2021-05-14 10:50:30 -0400

    serialized product data

--- backend/base/views.py
+++ backend/base/views.py
@@ -3,7 +3,9 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from .models import Product
 from .products import products
+from .serializers import ProductSerializer
 
 
 @api_view(['GET'])
@@ -27,7 +29,9 @@ def getRoutes(request):
 
 @api_view(['GET'])
 def getProducts(request):
-    return Response(products)
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
 
 
 @api_view(['GET'])
